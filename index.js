e361c6df176714cec807609e2c3eca79cfc8407f
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-14 13:16:53 -0600

    setting up prompts

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const inquirer = require('inquirer')
 
 
+
 inquirer
 .prompt([
     {
-        
-    }
-])
+      type: 'input',
+      name: 'logo',
+      message: 'Enter up to 3 characters',  
+    },
+    {
+        type: 'input',
+        name: ' text color',
+        message: 'What color would you like your text to be?',
+    },
+    {
+        type: 'list',
+        name: 'shapes',
+        choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+        type: 'input',
+        name: 'shape color',
+        message: 'Whate color would you like the shape to be?',
+    },
+// ]).then((response) =>
+// const TODO: MAKE THIS MAKE THE LOGO )
