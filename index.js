76ec67bf4ad2ec4f725e1893eae2edd33ce4e3a9
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-14 14:51:07 -0600

    writing the functions

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-const inquirer = require('inquirer')
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {circle, triangle, square} = require('./lib/shapes');
 
 
 inquirer
 .prompt([
     {
       type: 'input',
-      name: 'logo',
+      name: 'logoText',
       message: 'Enter up to 3 characters',  
     },
     {
         type: 'input',
-        name: ' text color',
+        name: ' textColor',
         message: 'What color would you like your text to be?',
     },
     {
         type: 'list',
-        name: 'shapes',
+        name: 'shapeType',
         choices: ["Circle", "Triangle", "Square"],
     },
     {
         type: 'input',
-        name: 'shape color',
+        name: 'shapeColor',
         message: 'Whate color would you like the shape to be?',
     },
-// ]).then((response) =>
-// const TODO: MAKE THIS MAKE THE LOGO )
+]).then((response) => {
+    const {logoText, textColor, shapeType, shapeColor} = response;
+    
+    let shape;
+    if (shapeType === 'Circle') {
+        shape = new circle();
+    } else if (shapeType === 'Triangle') {
+        shape = new triangle();
+    } else if (shapeType === 'Square') {
+        shape = new square();
+    }
+
+    shape.setColor(shapeColor);
+
+    const svgContent = `
+    ${shape.render()}
+    <text x="50%" y="50%" text-anchor="middle" dy=".3em" fill="${textColor}" font-size="20">${logoText}</text>
+    `;
+
+    fs.writeFileSync('logo.svg', svgContent);
+
+    console.log('Generated logo.svg')
+});
